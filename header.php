df7cc6786119805e1b197afaf3385e020643e0ec
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2013-10-29 16:20:45 +0700

    header: wp_title

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Bootstrap, from Twitter</title>
+    <title><?php wp_title(); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Le styles -->
