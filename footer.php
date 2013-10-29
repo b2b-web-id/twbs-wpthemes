23dc1c7bd16fddf5b6d358df2079826240ecc755
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2013-10-29 15:07:50 +0700

    header: wp_list_pages

--- footer.php
+++ footer.php
@@ -1,11 +1,12 @@
-      <hr>
+<hr>
 
-<footer><p>&copy; Company 2012</p></footer>
+<footer>
+ <p>&copy; Company 2012</p>
+</footer>
 
 </div> <!-- /container -->
 
-<script src="//cdn.kalamangga.net/pustaka/jquery/jquery-1.10.2.min.js"></script>
-<script src="//cdn.kalamangga.net/pustaka/bootstrap/v2.3.2/js/bootstrap.min.js"></script>
+<?php wp_footer(); ?>
 
 </body>
 
