d5b517b451475c2dc710b8b48d03b3e6374d2af2
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2013-10-29 16:28:52 +0700

    index: the_title, the_permalink, the_time

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	  	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<p><em><?php the_time(); ?></em></p>
+		<hr>
 	  	<?php the_content(); ?>
 
 	<?php endwhile; else: ?>
