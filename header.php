283440da13a09840e5cf26ed21c1b74cca9dc732
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2013-10-29 16:23:06 +0700

    header: wp_title

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title><?php wp_title(); ?></title>
+    <title><?php wp_title('|',1,'right'); ?> <?php bloginfo('name'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Le styles -->
