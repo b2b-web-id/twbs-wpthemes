ced75a0068746a9d8ff65cb462e31406b411a20e
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2013-10-30 00:29:30 +0700

    fixing

--- index.php
+++ index.php
@@ -18,29 +18,8 @@
 <?php endwhile; else: ?>
 <p><?php _e('Sorry, this page does not exist.'); ?></p>
 <?php endif; ?>
-<?php
-$postlist = get_posts('sort_column=menu_order&sort_order=asc');
-$posts = array();
-foreach ($postlist as $post) {
-   $posts[] += $post->ID;
-}
-$current = array_search(get_the_ID(), $posts);
-$prevID = $posts[$current-1];
-$nextID = $posts[$current+1];
-?>
-<div class="navigation">
-<?php if (!empty($prevID)) { ?>
-<div class="alignleft">
-<a href="<?php echo get_permalink($prevID); ?>"
-  title="<?php echo get_the_title($prevID); ?>">Previous</a>
-</div>
-<?php }
-if (!empty($nextID)) { ?>
-<div class="alignright">
-<a href="<?php echo get_permalink($nextID); ?>" 
- title="<?php echo get_the_title($nextID); ?>">Next</a>
-</div>
-<?php } ?>
+<div class="pull-left"><?php previous_post('&laquo; &laquo; %', '', 'yes'); ?></div>
+<div class="pull-right"><?php next_post('% &raquo; &raquo; ', '', 'yes'); ?></div>
 </div>
 <!-- Index -->
 </div>
