5ec766784aee69c5ded8f821834c1fd46b7b509e
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2013-10-29 15:16:09 +0700

    header: menu

--- header.php
+++ header.php
@@ -34,25 +34,21 @@
 <body>
 
 <div class="navbar navbar-inverse navbar-fixed-top">
-<div class="navbar-inner">
-
-<div class="container">
- <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-  <span class="icon-bar"></span>
-  <span class="icon-bar"></span>
-  <span class="icon-bar"></span>
- </a>
- <a class="brand" href="#"><?php bloginfo('title'); ?></a>
- <div class="nav-collapse collapse">
+ <div class="navbar-inner">
+  <div class="container">
+   <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+   </a>
+   <a class="brand" href="#"><?php bloginfo('title'); ?></a>
    <form class="navbar-form pull-right">
     <input class="span2" type="text" placeholder="Email">
     <input class="span2" type="password" placeholder="Password">
     <button type="submit" class="btn">Sign in</button>
    </form>
-  </div><!--/.nav-collapse -->
+  </div>
  </div>
-</div>
-
 </div>
 
 <div class="container">
