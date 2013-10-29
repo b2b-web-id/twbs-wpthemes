06aa332576bc725899478a28ef2a892941151ac5
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2013-10-29 15:23:07 +0700

    header: wpbootstrap

--- header.php
+++ header.php
@@ -1,54 +1,40 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="utf-8">
+    <title>Bootstrap, from Twitter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<head>
+    <!-- Le styles -->
+    <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
 
-<link rel="dns-prefecth" href="//kalamangga.net" />
-<link rel="dns-prefecth" href="//lifeteen.kalamangga.net" />
-<link rel="dns-prefecth" href="//lifeteen.com" />
 
-<meta charset="<?php bloginfo('charset'); ?>" />
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<meta name="description" content="<?php bloginfo('description'); ?>" />
-<meta name="keywords" content="Lifeteen" />
-<meta name="author" content="Yudha H Tejaningrat" />
-<meta name="generator" content="Nano Editor & GEdit" />
-<title><?php bloginfo('title'); ?></title>
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
 
-<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
-<link href="//cdn.kalamangga.net/pustaka/bootstrap/v2.3.2/css/bootstrap.css" rel="stylesheet">
-<style type="text/css">
- body {
-  padding-top: 60px;
-  padding-bottom: 40px;
- }
-</style>
-<link href="//cdn.kalamangga.net/pustaka/bootstrap/v2.3.2/css/bootstrap-responsive.css" rel="stylesheet">
+    <?php wp_enqueue_script("jquery"); ?>
+    <?php wp_head(); ?>
+  </head>
+  <body>
 
-<!--[if lt IE 9]>
- <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
+  <div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-inner">
+      <div class="container">
+        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </a>
+        <a class="brand" href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></a>
+        <div class="nav-collapse collapse">
+          <ul class="nav">
 
-</head>
+              <?php wp_list_pages(array('title_li' => '', 'exclude' => 4)); ?>
 
-<body>
-
-<div class="navbar navbar-inverse navbar-fixed-top">
- <div class="navbar-inner">
-  <div class="container">
-   <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-   </a>
-   <a class="brand" href="#"><?php bloginfo('title'); ?></a>
-   <form class="navbar-form pull-right">
-    <input class="span2" type="text" placeholder="Email">
-    <input class="span2" type="password" placeholder="Password">
-    <button type="submit" class="btn">Sign in</button>
-   </form>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
   </div>
- </div>
-</div>
 
-<div class="container">
+  <div class="container">
