95e49872f8930597e010bd2b864b0ce2558334dc
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2013-10-29 14:13:39 +0700

    Edit header & footer

--- footer.php
+++ footer.php
@@ -1,1 +1,12 @@
+      <hr>
+
+<footer><p>&copy; Company 2012</p></footer>
+
+</div> <!-- /container -->
+
+<script src="//cdn.kalamangga.net/pustaka/jquery/jquery-1.10.2.js"></script>
+<script src="//cdn.kalamangga.net/pustaka/bootstrap/v2.3.2/js/bootstrap.js"></script>
+
+</body>
+
 </html>
--- header.php
+++ header.php
@@ -3,12 +3,9 @@
 
 <head>
 
-<link rel="dns-prefecth" href="//yht.web.id" />
-<link rel="dns-prefecth" href="//labs.yht.web.id" />
-<link rel="dns-prefecth" href="//github.com" />
 <link rel="dns-prefecth" href="//kalamangga.net" />
-<link rel="dns-prefecth" href="//piwik.kalamangga.net" />
-<link rel="dns-prefecth" href="//creativecommons.org" />
+<link rel="dns-prefecth" href="//lifeteen.kalamangga.net" />
+<link rel="dns-prefecth" href="//lifeteen.com" />
 
 <meta charset="<?php bloginfo('charset'); ?>" />
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
@@ -19,21 +16,60 @@
 <title>Lifeteen Indonesia</title>
 
 <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
+<link href="//cdn.kalamangga.net/pustaka/bootstrap/v2.3.2/css/bootstrap.css" rel="stylesheet">
+<style type="text/css">
+ body {
+  padding-top: 60px;
+  padding-bottom: 40px;
+ }
+</style>
+<link href="//cdn.kalamangga.net/pustaka/bootstrap/v2.3.2/css/bootstrap-responsive.css" rel="stylesheet">
 
-<?php
-//if ( is_singular() && get_option('thread_comments'))
-// wp_unqueue_script('comment_reply');
-//wp_head();
-?>
+<!--[if lt IE 9]>
+ <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
 
 </head>
 
 <body>
 
-<div id="wrapper">
+<div class="navbar navbar-inverse navbar-fixed-top">
+<div class="navbar-inner">
 
-<div id="header">
- <h1>HEADER</h1>
+<div class="container">
+ <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+  <span class="icon-bar"></span>
+  <span class="icon-bar"></span>
+  <span class="icon-bar"></span>
+ </a>
+ <a class="brand" href="#">Project name</a>
+ <div class="nav-collapse collapse">
+  <ul class="nav">
+   <li class="active"><a href="#">Home</a></li>
+   <li><a href="#about">About</a></li>
+   <li><a href="#contact">Contact</a></li>
+   <li class="dropdown">
+     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+     <ul class="dropdown-menu">
+      <li><a href="#">Action</a></li>
+      <li><a href="#">Another action</a></li>
+      <li><a href="#">Something else here</a></li>
+      <li class="divider"></li>
+      <li class="nav-header">Nav header</li>
+      <li><a href="#">Separated link</a></li>
+      <li><a href="#">One more separated link</a></li>
+     </ul>
+    </li>
+   </ul>
+   <form class="navbar-form pull-right">
+    <input class="span2" type="text" placeholder="Email">
+    <input class="span2" type="password" placeholder="Password">
+    <button type="submit" class="btn">Sign in</button>
+   </form>
+  </div><!--/.nav-collapse -->
  </div>
+</div>
 
+</div>
 
+<div class="container">
