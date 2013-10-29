928159fca4744cd09ffe1aad40f9353daaf39eb8
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2013-10-29 15:12:58 +0700

    header: [-] wp_list_pages

--- header.php
+++ header.php
@@ -44,9 +44,6 @@
  </a>
  <a class="brand" href="#"><?php bloginfo('title'); ?></a>
  <div class="nav-collapse collapse">
-  <ul class="nav">
-<?php wp_list_pages(array('title_li',=>'')); ?>
-  </ul>
    <form class="navbar-form pull-right">
     <input class="span2" type="text" placeholder="Email">
     <input class="span2" type="password" placeholder="Password">
