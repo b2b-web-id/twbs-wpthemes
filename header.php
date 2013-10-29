96d2fb5dd60277c99dc683b0ba3f4d9089e2bf48
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2013-10-29 17:50:52 +0700

    header: try to fix

--- header.php
+++ header.php
@@ -5,16 +5,12 @@
     <title><?php wp_title('|',1,'right'); ?> <?php bloginfo('name'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Le styles -->
     <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
 
-
-    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
 
-    <?php wp_enqueue_script("jquery"); ?>
     <?php wp_head(); ?>
   </head>
   <body>
--- page.php
+++ page.php
@@ -7,16 +7,14 @@
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<h1><?php the_title(); ?></h1>
 	  	<?php the_content(); ?>
-
 	<?php endwhile; else: ?>
 		<p><?php _e('Sorry, this page does not exist.'); ?></p>
 	<?php endif; ?>
 
   </div>
   <div class="span4">
-	<h2>Sidebar</h2>	
+	<?php get_sidebar(); ?>	
   </div>
 </div>
-
 
 <?php get_footer(); ?>
