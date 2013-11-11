c7e16e3ea1df282bf897c68bd5170e803a20af0a
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2013-11-11 17:43:22 +0700

    Piwik Tracking Code

--- footer.php
+++ footer.php
@@ -5,5 +5,23 @@
 </footer>
 </div>
 <?php wp_footer(); ?>
+
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(["setDomains", ["*.b2b.web.id","*.direktori.b2b.web.id","*.panas.b2b.web.id"]]);
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
+
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.kalamangga.net/";
+    _paq.push(["setTrackerUrl", u+"piwik.php"]);
+    _paq.push(["setSiteId", "3"]);
+    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
+
 </body>
 </html>
